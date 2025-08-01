e0518abb2e488e94c37687faca3fe653c69f109d
JMag <jmaghamez@gmail.com>
2025-07-31 20:26:58 -0400

    Updated files

--- src/main/kotlin/FunctionExperiment.kt
+++ src/main/kotlin/FunctionExperiment.kt
@@ -1,18 +1,15 @@
 package org.example
 
-fun main()
-{
-    println(performCombatAction(playerIsBoosted = false, enemyEncounterStatus = true))
-}
+fun main() { println(performCombatAction(playerIsBoosted = false, enemyEncounterStatus = true)) }
 
 fun performCombatAction(
-    enemyName: String = "Darth Vader",
-    playerIsBoosted: Boolean,
-    enemyEncounterStatus: Boolean): String
+  enemyName: String = "Darth Vader",
+  playerIsBoosted: Boolean,
+  enemyEncounterStatus: Boolean): String
 {
-    return when(playerIsBoosted && enemyEncounterStatus || enemyEncounterStatus)
-    {
-        true -> "You are attacking $enemyName with 2X normal health."
-        else -> "No enemies found."
-    }
+  return when(playerIsBoosted && enemyEncounterStatus || enemyEncounterStatus)
+  {
+    true -> "You are attacking $enemyName with 2X normal health."
+    else -> "No enemies found."
+  }
 }
